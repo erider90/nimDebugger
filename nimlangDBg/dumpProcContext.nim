e30ca2d70c93e61dc1b11316a86fc4f894df2b99
erider90 <69161381+erider90@users.noreply.github.com>
2024-12-08 22:06:33 -0500

    Update dumpProcContext.nim
    fixing somethings: todo fix hardcoded file

--- nimlangDBg/dumpProcContext.nim
+++ nimlangDBg/dumpProcContext.nim
@@ -1,37 +1,35 @@
-#import windef 
 import winINC
-# import winlean 
 
-# const FALSE* = 0
-
+const FALSE* = 0
 
 proc main(): void =
-  # var si: STARTUPINFO = [0]
-  # var si: STARTUPINFO 
-  var si: winINC.STARTUPINFO
-  #var pi: PROCESS_INFORMATION
-  var pi: winINC.PROCESS_INFORMATION
-  
-  ##  Create a new process
-  winINC.CreateProcessA*(nil, "./a.exe", nil, nil, false, 0, nil, nil, si, pi)
-  
-  ##  Get the first thread handle
-  var hThread: HANDLE = OpenThread(THREAD_ALL_ACCESS, false, GetThreadId(pi.hThread))
-  ##  Get the thread context
-  var ctx: CONTEXT
-  ctx.ContextFlags = CONTEXT_FULL
-  if not GetThreadContext(hThread, addr(ctx)):
-    printf("Failed to get thread context\n")
-    return 1
-  printf("EAX: 0x%X\n", ctx.Rax)
-  printf("EBX: 0x%X\n", ctx.Rbx)
-  printf("ECX: 0x%X\n", ctx.Rcx)
-  printf("EDX: 0x%X\n", ctx.Rdx)
-  printf("ESP: 0x%X\n", ctx.Rsp)
-  printf("EBP: 0x%X\n", ctx.Rbp)
-  printf("EIP: 0x%X\n", ctx.Rip)
-  ##  Close handles
-  CloseHandle(hThread)
-  CloseHandle(pi.hProcess)
-  CloseHandle(pi.hThread)
+    # var si: STARTUPINFO = [0]
+    # var si: STARTUPINFO 
+    var si: winINC.STARTUPINFO
+    #var pi: PROCESS_INFORMATION
+    var pi: winINC.PROCESS_INFORMATION
 
+    ##  Create a new process
+    var check = createProcessA(nil, "C:/Users/super/Downloads/REtools/nimTools/a.exe", nil, nil, false, 0, nil, nil, si, pi)
+    ##  Get the first thread HANDLE
+    var hThread: HANDLE = OpenThread(THREAD_ALL_ACCESS, FALSE, GetThreadId(pi.hThread))
+    ##  Get the thread context
+    var ctx: CONTEXT
+    ctx.ContextFlags = CONTEXT_FULL
+    
+    if GetThreadContext(hThread, addr(ctx)) == false :
+        printf("Failed to get thread context\n")
+    else: 
+        printf("EAX: 0x%X\n", ctx.Rax)
+        printf("EBX: 0x%X\n", ctx.Rbx)
+        printf("ECX: 0x%X\n", ctx.Rcx)
+        printf("EDX: 0x%X\n", ctx.Rdx)
+        printf("ESP: 0x%X\n", ctx.Rsp)
+        printf("EBP: 0x%X\n", ctx.Rbp)
+        printf("EIP: 0x%X\n", ctx.Rip)
+        ##  Close HANDLEs
+        discard CloseHandle(hThread)
+        discard CloseHandle(pi.hProcess)
+        discard CloseHandle(pi.hThread)
+
+main() 
